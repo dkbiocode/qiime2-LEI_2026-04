31acac8f0113516cf8ce4b9829b7956d8c6eb666
David King <davidcking.inbox@gmail.com>
2026-04-28 12:47:26 -0600

    processed through 2-denoise

--- 2-denoise.sh
+++ 2-denoise.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -eu
+INDENT=""
+echo_grey() {
+    echo -e "$INDENT\033[0;90m$@\033[0m"
+}
+
 threads=$(nproc)
 
 # Step 1: dada2 denoise
@@ -8,11 +13,6 @@ input_qza=demux.qza
 rep_seqs_qza=2-repseqs.qza
 stats_qza=2-stats.qza
 table=2-table.qza
-
-
-# Step 3: feature-table tabulate-seqs
-
-rep_seqs_qzv=${rep_seqs_qza/.qza/.qzv}
 
 # STEP 1: DENOISE
 cmd="qiime dada2 denoise-paired \
@@ -25,7 +25,7 @@ cmd="qiime dada2 denoise-paired \
     --o-representative-sequences ${rep_seqs_qza} \
     --o-denoising-stats ${stats_qza} \
     --o-table ${table}"
-echo $cmd
+echo_grey $cmd
 #time eval $cmd
 
 # STEP 2: TABULATE
@@ -40,39 +40,42 @@ stats_qzv=${stats_qza/.qza/.qzv}
 cmd="qiime metadata tabulate \
     --m-input-file ${stats_qza}  \
     --o-visualization ${stats_qzv}"
-echo $cmd
-time eval $cmd
-
-exit 0
+echo_grey $cmd
+#time eval $cmd
 
 # STEP 3: feature-table SUMMARIZE:
+summarize_qvz="summarize.qzv"
 if [ -e metadata.txt ]
 then
     echo "metadata.txt already exists. Rename or delete to recreate it."
 else
-    echo "sample_id" > metadata.txt
+    echo -e "sampleid\tdescription" > metadata.txt
     cmd="ls Fastq/*_R1_001.fastq.gz \
     | xargs -n1 basename \
     | sed 's/_S[0-9]*_L001_R1_001.fastq.gz//' \
-    >> metadata.txt"
-    echo "$cmd"
+    | awk '{print \$0\"\tna\"}' >> metadata.txt"
+    echo_grey "$cmd"
     eval "$cmd"
 
-    echo "Created metadata.txt"
+    echo -e "\033[0;90mCreated metadata.txt"
     cat metadata.txt
+    echo -e "\033[0m"
 fi
 cmd="qiime feature-table summarize \
-        --i-table {input.table} \
-        --m-sample-metadata-file {input.metadata} \
-        --o-visualization {output}"
+        --i-table ${table} \
+        --m-sample-metadata-file metadata.txt \
+        --o-visualization ${summarize_qvz}"
+echo_grey "$cmd"
+#time eval "$cmd"
 
 
 # STEP 4: TABULATE_SEQS:
-    # input:
-    #     f"{WORK_DIR}/02_repSeqs_mouse_fecal_samples.qza"
-    # output:
-    #     f"{WORK_DIR}/02_repSeqs_mouse_fecal_samples.qzv"
+# INPUT: rep_seqs_qza # from step 1: --o-representative-sequences ${rep_seqs_qza}
+# OUTPUT
+rep_seqs_qzv=${rep_seqs_qza/.qza/.qzv}
+
 cmd="qiime feature-table tabulate-seqs \
-    --i-data {input} \
-    --o-visualization {output}"
-echo $cmd
+    --i-data ${rep_seqs_qza} \
+    --o-visualization ${rep_seqs_qzv}"
+echo_grey $cmd
+time eval "$cmd"
